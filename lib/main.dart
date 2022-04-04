238e054553ab053d153ed9c72ee9d1a97c433444
prachi0606 <prachipanwar0606@gmail.com>
2022-04-04 19:26:58 +0530

    changed height to 0.83

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class HomePage extends StatelessWidget {
       ),
       body:Container(
         padding: const EdgeInsets.all(8.0),
-        height: MediaQuery.of(context).size.height * 0.84,
+        height: MediaQuery.of(context).size.height * 0.83,
         width: MediaQuery.of(context).size.width * 1.0,
         margin: const EdgeInsets.only(left: 20.0,right: 20.0,bottom: 40.0),
         decoration: BoxDecoration(
